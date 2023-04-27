6b4825ca8aa7a5f2204ff08b46cc8a7a5d72c5a2
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-04-27 17:29:03 +0200

    Update DemoSpringJavaApplication.java

--- src/main/java/top/abr/demo_spring_java/DemoSpringJavaApplication.java
+++ src/main/java/top/abr/demo_spring_java/DemoSpringJavaApplication.java
@@ -2,12 +2,17 @@ package top.abr.demo_spring_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoSpringJavaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoSpringJavaApplication.class, args);
     }
-
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, world!";
+    }
 }
