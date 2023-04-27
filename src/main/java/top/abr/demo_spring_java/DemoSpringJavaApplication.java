66572de76ef8d84ebd3a97410c35e714225d3105
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-04-27 17:23:17 +0200

    init

---
+++ src/main/java/top/abr/demo_spring_java/DemoSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package top.abr.demo_spring_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringJavaApplication.class, args);
+    }
+
+}
